81b782ab39d38884f85cb57c4a09fc21de238b38
Christopher Mason <crstffr@gmail.com>
2014-09-22 16:26:53 -0500

    Added panel application

---
+++ core-firmware/applications/panel/app_panel.cpp
@@ -0,0 +1,43 @@
+#include "application.h"
+#include "../_inc/core-common.h"
+#include "../_inc/component-button.h"
+#include "../_inc/component-quadencoder.h"
+#include "../_inc/component-pir.h"
+
+// ******************************
+// Class instantiation
+// ******************************
+
+PIR pir("pir", A0);
+QuadEncoder knob("knob", D0, D1);
+Button btn0("btn0", D2, INPUT_PULLDOWN);
+Button btn1("btn1", D3, INPUT_PULLDOWN);
+Button btn2("btn2", D4, INPUT_PULLDOWN);
+Button btn3("btn3", D5, INPUT_PULLDOWN);
+Button btn4("btn4", D6, INPUT_PULLDOWN);
+
+void setup() {
+
+    coreSetup();
+    mqttSetup(DEVICE_TYPE_PANEL);
+
+}
+
+// ******************************
+// Main Loop
+// ******************************
+
+void loop() {
+
+    mqttLoop();
+    knob.loop();
+    btn0.loop();
+    btn1.loop();
+    btn2.loop();
+    btn3.loop();
+    btn4.loop();
+    pir.loop();
+
+}
+
+void mqttCustomMessageHandler(char* topic, char** topicParts, int topicCount, char* msg) {}
